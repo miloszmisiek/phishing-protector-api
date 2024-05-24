b5c08cc665171274e98f208a32d3151ea86d1044
Milosz Misiek <84859537+miloszmisiek@users.noreply.github.com>
2024-05-24 19:57:02 +0200

    fix: add x to allowed origins

--- app/server/app.py
+++ app/server/app.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from app.server.routes.urls import router as UrlsRouter
 
-origins = ['https://twitter.com',
+origins = ['https://x.com','https://twitter.com',
            'chrome-extension://gefeonighpcjgmdlhanednlikmdabpfg', 'chrome-extension://kkmfcnoeffmmfiplcjmfciphjkibfekg']
 
 app = FastAPI()
